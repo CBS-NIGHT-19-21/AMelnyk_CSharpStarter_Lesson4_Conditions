76adef2ce0cef3d1a1587e89e0aa0172da114f4f
Anatolii_M <mapl175g@gmail.com>
2021-12-07 15:01:37 +0200

    Add project files.

---
+++ Additional_task/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+// Создать проект по шаблону Console Application. 
+// Написать программу русско-английский переводчик. Программа знает 10 слов о погоде. Требуется,
+// чтобы пользователь вводил слово на русском языке, а программа давала ему перевод этого слова на 
+// английском языке. Если пользователь ввел слово, для которого отсутствует перевод, то следует вывести 
+// сообщение, что такого слова нет.
+
+namespace Additional_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово о погоде");
+            
+            //Вод и инициализация переменной.
+            string weather = Console.ReadLine();
+            // Для перевода введенного слова, используем переключатель switch-case.
+            switch (weather)
+            {
+                case "облако":
+                    {
+                        Console.WriteLine("Cloud");
+                        break;
+                    }
+                case "туман":
+                    {
+                        Console.WriteLine("Fog");
+                        break;
+                    }
+                case "солнце":
+                    {
+                        Console.WriteLine("Sun");
+                        break;
+                    }
+                case "дождь":
+                    {
+                        Console.WriteLine("Rain");
+                        break;
+                    }
+                case "мороз":
+                    {
+                        Console.WriteLine("Frost");
+                        break;
+                    }
+                case "снег":
+                    {
+                        Console.WriteLine("Snow");
+                        break;
+                    }
+                case "ветер":
+                    {
+                        Console.WriteLine("Wind");
+                        break;
+                    }
+                case "ураган":
+                    {
+                        Console.WriteLine("Storm");
+                        break;
+                    }
+                case "гроза":
+                    {
+                        Console.WriteLine("Thunderstorm");
+                        break;
+                    }
+                case "буря":
+                    {
+                        Console.WriteLine("Hurricane");
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Введенное слово отсутствует в словаре");
+                        break;
+                    }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+// Создайть проект по шаблону Console Application. 
+// Написать программу - консольный калькулятор. 
+// Создать две переменные с именами operand1 и operand2 с произвольными значениями.
+// Предложить пользователю ввести знак арифметической операции. Примите 
+// значение, введенное пользователем, и поместите его в строковую переменную sign. 
+// Для организации выбора алгоритма вычислительного процесса, использовать переключатель switch. 
+// Вывести на экран результат выполнения арифметической операции. 
+// В случае использования операции деления, организовать проверку попытки деления на ноль. И если 
+// таковая имеется, то отмените выполнение арифметической операции и уведомите об ошибке 
+//пользователя.
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод и инициализация переменных.
+            Console.WriteLine("Введите первое число");
+            string myOperand1 = Console.ReadLine();
+            double operand1 = Convert.ToDouble(myOperand1);
+
+            Console.WriteLine("Введите второе число");
+            string myOperand2 = Console.ReadLine();
+            double operand2 = Convert.ToDouble(myOperand2);
+            Console.WriteLine("Введите арифметическую операцию");
+            char sing = Convert.ToChar(Console.ReadLine());
+
+            // Для организации выбора алгоритма вычислительного процесса, используем переключатель switch-case.
+            switch (sing)
+            {
+                case '+':
+                    {
+                        Console.WriteLine($"Результат сложения чисел = {operand1 + operand2}");
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine($"Результат вычитания чисел = {operand1-operand2}");
+                        break;
+                    }
+                case '*':
+                    {
+                    Console.WriteLine($"Результат умножения чисел = {operand1*operand2}");
+                        break;
+                    }
+                case '/':
+                    {
+                        //Проверка деления на ноль.
+                        if (operand2 == 0)
+                        {
+                            Console.WriteLine($"Ошибка деления на ноль");
+                        }
+                        Console.WriteLine($"Результат деления чисел = {operand1/operand2}");
+                        break;
+                    }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
